2332ba423ead0955bec3bea251531144b7e85dc8
Apolo Freitas <apolobrener11@gmail.com>
2021-03-09 20:05:08 -0400

    Refactor home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,44 @@
-import DefaultLayout from "@components/DefaultLayout";
-import config from "@config";
+import DefaultLayout from '@components/DefaultLayout'
+import Link from '@components/Link'
+import config from '@config'
+import styles from '@styles/pages/index.module.css'
 
 export default function Home() {
   return (
     <DefaultLayout title={config.title} description={config.description}>
-      <center>
-      <title>Niobio</title>
-      <h1>Site Oficial em Construção!</h1>
-      <h1>Niobio - Dinheiro Transparente</h1>
-      <h1></h1>
-      <h2>A quantidade máxima de moedas é de 210.000 unidades</h2>
-      <h3><Link ink href="https://niobio.org" ><a><u>Consultar saldos</u></a></Link></h3>
-      <h3><Link ink href="https://niobio.cash"><a><u>Explorer</u></a></Link></h3>
-      <h3>Redes Sociais:</h3>
-      <h4><Link ink href="https://www.facebook.com/niobio.dinheiro"><a><u>Facebook</u></a></Link></h4>
-      <h4><Link ink href="https://twitter.com/niobio_dinheiro/"><a><u>Twitter</u></a></Link></h4>
-      <h4><Link ink href="https://www.youtube.com/user/melecadosapo/"><a><u>Canal do Marconi no Youtube</u></a></Link></h4>  
-      <h2>#SemEnrolação #NovoNiobio #goNBR #ToTheMoon</h2>
-      </center>      
+      <div class={styles.container}>
+        <title>Niobio</title>
+        <h1>Site Oficial em Construção!</h1>
+        <h1>Niobio - Dinheiro Transparente</h1>
+        <h2>A quantidade máxima de moedas é de 210.000 unidades</h2>
+        <h3>
+          <Link href="https://niobio.org">
+            <u>Consultar saldos</u>
+          </Link>
+        </h3>
+        <h3>
+          <Link href="https://niobio.cash">
+            <u>Explorer</u>
+          </Link>
+        </h3>
+        <h3>Redes Sociais:</h3>
+        <h4>
+          <Link href="https://www.facebook.com/niobio.dinheiro">
+            <u>Facebook</u>
+          </Link>
+        </h4>
+        <h4>
+          <Link href="https://twitter.com/niobio_dinheiro/">
+            <u>Twitter</u>
+          </Link>
+        </h4>
+        <h4>
+          <Link href="https://www.youtube.com/user/melecadosapo/">
+            <u>Canal do Marconi no Youtube</u>
+          </Link>
+        </h4>
+        <h2>#SemEnrolação #NovoNiobio #goNBR #ToTheMoon</h2>
+      </div>
     </DefaultLayout>
-  );
+  )
 }
